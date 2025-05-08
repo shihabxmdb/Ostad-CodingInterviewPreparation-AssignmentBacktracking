3976a617965dd03e44ae533a4e025882f65d5027
Shihab Hasan <shihab.xm.db@gmail.com>
2025-05-08 21:35:22 +0600

    updated

--- KNights/Program.cs
+++ KNights/Program.cs
@@ -1,1 +1,65 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static int maxCount = 0;
+    static void Main()
+    {
+        Console.Write("Enter  size : ");
+        int n = int.Parse(Console.ReadLine());
+        int result = MaxKnights(n);
+        Console.WriteLine("Maximum knights : " + result);
+    }
+    static int MaxKnights(int n)
+    {
+        int[,] board = new int[n, n];
+        maxCount = 0;
+        Solve(board, n, 0, 0, 0);
+        return maxCount;
+    }
+
+    static void Solve(int[,] board, int n, int row, int col, int count)
+    {
+        if (row == n)
+        {
+            maxCount = Math.Max(maxCount, count);
+            return;
+        }
+
+        int nextRow = (col + 1 == n) ? row + 1 : row;
+        int nextCol = (col + 1) % n;
+
+        // Try placing a knight
+        if (IsSafe(board, row, col, n))
+        {
+            board[row, col] = 1;
+            Solve(board, n, nextRow, nextCol, count + 1);
+            board[row, col] = 0; 
+        }
+
+        //W
+        //ithout placing a knight
+        Solve(board, n, nextRow, nextCol, count);
+    }
+
+    static bool IsSafe(int[,] board, int row, int col, int n)
+    {
+      
+        if (row + 2 < n && col - 1 >= 0 && board[row + 2,col - 1] == 1) return false;
+        if (row + 2 < n && col +1 < n && board[row + 2, col + 1] == 1) return false;
+
+        if (row - 2 >=0 && col - 1 >=0 && board[row - 2, col - 1] == 1) return false;
+        if (row - 2 >=0 && col + 1 < n && board[row - 2, col + 1] == 1) return false;
+     
+        if (col - 2 >=0 && row - 1 >= 0 && board[row - 1, col - 2] == 1) return false;
+        if (col - 2 >=0 && row + 1 < n && board[row + 1, col - 2] == 1) return false;
+
+        if (row - 1 >= 0 && col + 2 < n && board[row - 1, col + 2] == 1) return false;
+        if (row + 1 < n && col + 2 < n && board[row +1, col + 2] == 1) return false;
+        return true;
+    }
+
+
+
+
+}
--- NQueens/Program.cs
+++ NQueens/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    
+    static int count = 0;
+    
+    public static void Main(string[] args)
+    {
+       
+        int n = int.Parse(Console.ReadLine());
+        int[,] board = new int[n, n];
+        Call(0, n,board);
+        Console.WriteLine("Total Solutions=" + count);
+
+    }
+
+
+    static void Call(int r, int n, int[,] board)
+    {
+        if (r == n)
+        {
+            count++;
+            return;
+        }
+
+        for (int c = 0; c < n; c++)
+        {
+            if (IsSafe(r, c, n, board))
+            {
+                board[r, c] = 1;
+                Call(r + 1, n,board);
+                board[r, c] = 0; //  Backtrack
+                //return;
+            }
+
+            
+        }
+         return;
+    }
+
+    static bool IsSafe(int r, int c, int n, int[,] board)
+        {
+            //down check
+            int r1 = r;
+            while (r1 >= 0)
+            {
+                if (board[r1, c] == 1)
+                {
+                    return false;
+                }
+                r1--;
+            }
+          
+            //left diagonal check
+            r1 = r;
+            int c1 = c;
+            while (r1 >= 0 && c1 >= 0)
+            {
+                if (board[r1, c1] == 1)
+                {
+                    return false;
+                }
+                r1--;
+                c1--;
+            }
+            //right diagonal check
+            r1 = r;
+            c1 = c;
+            while (r1 >= 0 && c1 < n)
+            {
+                if (board[r1, c1] == 1)
+                {
+                    return false;
+                }
+                r1--;
+                c1++;
+            }
+
+            return true;
+        }
+    
+}
